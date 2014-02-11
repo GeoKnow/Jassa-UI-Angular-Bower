e61408fbbdd5817c99a18577d581134fb7c6a388
cstadler <cstadler@informatik.uni-leipzig.de>
2014-02-11 18:30:18 +0100

    Updating version 0.0.1-SNAPSHOT with tag v0.0.1-SNAPSHOT

--- jassa-ui-angular-tpls.js
+++ jassa-ui-angular-tpls.js
@@ -213,7 +213,7 @@ angular.module("template/facettree/facet-dir-content.html", []).run(["$templateC
     "\n" +
     "<div style=\"padding-left: {{16* (dirset.path.getLength()+ 1)\"\n" +
     "	ng-repeat=\"facet in dirset.children\"\n" +
-    "	ng-include=\"'resources/partials/facet-tree-item.html'\" ></div>\n" +
+    "	ng-include=\"'template/facettree/facet-tree-item.html'\" ></div>\n" +
     "");
 }]);
 
@@ -236,10 +236,10 @@ angular.module("template/facettree/facet-tree-item.html", []).run(["$templateCac
     "\n" +
     "		<tabset class=\"tabset-small\">\n" +
     "			<tab heading=\"Incoming Facets\" active=\"{{facet.isIncomingActive === true}}\" select=\"selectIncoming(facet.item.getPath())\">\n" +
-    "				<div ng-repeat=\"dirset in [facet.incoming]\" ng-include=\"'resources/partials/facet-dir-content.html'\"></div>\n" +
+    "				<div ng-repeat=\"dirset in [facet.incoming]\" ng-include=\"'template/facettree/facet-dir-content.html'\"></div>\n" +
     "			</tab>\n" +
     "			<tab heading=\"Outgoing Facets\" active=\"{{facet.isOutgoingActive === true}}\" select=\"selectOutgoing(facet.item.getPath())\">					\n" +
-    "				<div ng-repeat=\"dirset in [facet.outgoing]\" ng-include=\"'resources/partials/facet-dir-content.html'\"></div>\n" +
+    "				<div ng-repeat=\"dirset in [facet.outgoing]\" ng-include=\"'template/facettree/facet-dir-content.html'\"></div>\n" +
     "			</tab>\n" +
     "		</tabset>\n" +
     "	</div>\n" +
