e255c7e68ae40a5bc9938b991c5abb7437ec8bdb
cstadler <cstadler@informatik.uni-leipzig.de>
2014-02-13 13:24:37 +0100

    Updating version 0.0.1-SNAPSHOT with tag v0.0.1-SNAPSHOT

--- jassa-ui-angular-tpls.js
+++ jassa-ui-angular-tpls.js
@@ -413,22 +413,22 @@ angular.module('ui.jassa.facettree', [])
 
 angular.module("template/facet-value-list/facet-value-list.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/facet-value-list/facet-value-list.html",
-    "		<div class=\"frame\">\n" +
-    "			<form ng-submit=\"filterTable(filterText)\">\n" +
-    "			    <input type=\"text\" ng-model=\"filterText\" />\n" +
-    "				<input class=\"btn-primary\" type=\"submit\" value=\"Filter\" />\n" +
-    "			</form>\n" +
-    "			<table>\n" +
-    "                <tr><th>Value</th><th>Constrained</th></tr>\n" +
+    "<div class=\"frame\">\n" +
+    "	<form ng-submit=\"filterTable(filterText)\">\n" +
+    "	    <input type=\"text\" ng-model=\"filterText\" />\n" +
+    "		<input class=\"btn-primary\" type=\"submit\" value=\"Filter\" />\n" +
+    "	</form>\n" +
+    "	<table>\n" +
+    "              <tr><th>Value</th><th>Constrained</th></tr>\n" +
     "<!-- <th>Count</th> -->\n" +
-    "			    <tr ng-repeat=\"item in facetValues\">\n" +
-    "                    <td>{{item.displayLabel}}</td>\n" +
+    "	    <tr ng-repeat=\"item in facetValues\">\n" +
+    "                  <td>{{item.displayLabel}}</td>\n" +
     "<!--                    <td>todo</td> -->\n" +
-    "                    <td><input type=\"checkbox\" ng-model=\"item.tags.isConstrainedEqual\" ng-change=\"toggleConstraint(item)\" /></td>\n" +
-    "                </tr>\n" +
-    "        	</table>\n" +
-    "    		<pagination class=\"pagination-small\" total-items=\"totalItems\" page=\"$parent.currentPage\" max-size=\"maxSize\" boundary-links=\"true\" rotate=\"false\" num-pages=\"numPages\"></pagination>\n" +
-    "		</div>\n" +
+    "                  <td><input type=\"checkbox\" ng-model=\"item.tags.isConstrainedEqual\" ng-change=\"toggleConstraint(item)\" /></td>\n" +
+    "              </tr>\n" +
+    "      	</table>\n" +
+    "  		<pagination class=\"pagination-small\" total-items=\"$parent.totalItems\" page=\"$parent.currentPage\" max-size=\"$parent.maxSize\" boundary-links=\"true\" rotate=\"false\" num-pages=\"$parent.numPages\"></pagination>\n" +
+    "</div>\n" +
     "");
 }]);
 
