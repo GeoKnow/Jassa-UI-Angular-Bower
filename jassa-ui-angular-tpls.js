43ca676c56ba9c809280bf8bcc7bec7995c24bc3
cstadler <cstadler@informatik.uni-leipzig.de>
2014-02-13 12:42:48 +0100

    Updating version 0.0.1-SNAPSHOT with tag v0.0.1-SNAPSHOT

--- jassa-ui-angular-tpls.js
+++ jassa-ui-angular-tpls.js
@@ -5,13 +5,178 @@
  * Version: 0.0.1-SNAPSHOT - 2014-02-13
  * License: MIT
  */
-angular.module("ui.jassa", ["ui.jassa.tpls", "ui.jassa.constraint-list","ui.jassa.facetValueList","ui.jassa.facettree"]);
+angular.module("ui.jassa", ["ui.jassa.tpls", "ui.jassa.constraint-list","ui.jassa.facettree","ui.jassa.facetvaluelist"]);
 angular.module("ui.jassa.tpls", ["template/facettree/facet-dir-content.html","template/facettree/facet-tree-item.html"]);
 angular.module('ui.jassa.constraint-list', [])
 
 ;
 
-angular.module('ui.jassa.facetValueList', [])
+angular.module('ui.jassa.facettree', [])
+
+/**
+ * Controller for the SPARQL based FacetTree
+ * Supports nested incoming and outgoing properties
+ *
+ */
+.controller('FacetTreeCtrl', ['$rootScope', '$scope', '$q', function($rootScope, $scope, $q) {
+        
+    var self = this;
+      
+      
+    var updateFacetTreeService = function() {
+        var isConfigured = $scope.sparqlService && $scope.facetTreeConfig;
+        //debugger;
+        $scope.facetTreeService = isConfigured ? Jassa.facete.FaceteUtils.createFacetTreeService($scope.sparqlService, $scope.facetTreeConfig, null) : null;
+    };
+    
+    var update = function() {
+        updateFacetTreeService();
+        //controller.refresh();
+        self.refresh();
+    };
+    
+    $scope.$watch('sparqlService', function() {
+    //console.log('args', $scope.sparqlService);
+        update();
+    });
+    
+    $scope.$watch('facetTreeConfig.hashCode()', function() {
+        update();
+    }, true);
+              
+      
+    $scope.doFilter = function(path, filterString) {
+        $scope.facetTreeConfig.getPathToFilterString().put(path, filterString);
+        self.refresh();
+    };
+    
+    self.refresh = function() {
+                  
+        var facet = $scope.facet;
+        var startPath = facet ? facet.item.getPath() : new facete.Path();
+    
+        if($scope.facetTreeService) {
+          
+            var facetTreeTagger = Jassa.facete.FaceteUtils.createFacetTreeTagger($scope.facetTreeConfig.getPathToFilterString());
+    
+            //console.log('scopefacets', $scope.facet);             
+            var promise = $scope.facetTreeService.fetchFacetTree(startPath);
+              
+            Jassa.sponate.angular.bridgePromise(promise, $q.defer(), $rootScope).then(function(data) {
+                facetTreeTagger.applyTags(data);
+                $scope.facet = data;
+            });
+    
+        } else {
+            $scope.facet = null;
+        }
+    };
+              
+    $scope.toggleCollapsed = function(path) {
+        Jassa.util.CollectionUtils.toggleItem($scope.facetTreeConfig.getExpansionSet(), path);
+          
+        var val = $scope.facetTreeConfig.getExpansionMap().get(path);
+        if(val == null) {
+            $scope.facetTreeConfig.getExpansionMap().put(path, 1);
+        }
+          
+        self.refresh();
+    };
+      
+    $scope.selectIncoming = function(path) {
+        console.log('Incoming selected at path ' + path);
+        if($scope.facetTreeConfig) {
+            var val = $scope.facetTreeConfig.getExpansionMap().get(path);
+            if(val != 2) {
+                $scope.facetTreeConfig.getExpansionMap().put(path, 2);
+                self.refresh();
+            }
+        }
+    };
+      
+    $scope.selectOutgoing = function(path) {
+        console.log('Outgoing selected at path ' + path);
+        if($scope.facetTreeConfig) {
+            var val = $scope.facetTreeConfig.getExpansionMap().get(path);
+            if(val != 1) {
+                $scope.facetTreeConfig.getExpansionMap().put(path, 1);
+                self.refresh();
+            }
+        }
+    };
+      
+      
+    $scope.selectFacetPage = function(page, facet) {
+        var path = facet.item.getPath();
+        var state = $scope.facetTreeConfig.getFacetStateProvider().getFacetState(path);
+        var resultRange = state.getResultRange();
+          
+        console.log('Facet state for path ' + path + ': ' + state);
+            var limit = resultRange.getLimit() || 0;
+              
+            var newOffset = limit ? (page - 1) * limit : null;
+              
+            resultRange.setOffset(newOffset);
+            
+            self.refresh();
+        };
+          
+        $scope.toggleSelected = function(path) {
+            $scope.onSelect({path: path});
+        };
+  
+        $scope.toggleTableLink = function(path) {
+            //$scope.emit('facete:toggleTableLink');
+        tableMod.togglePath(path);
+      
+        //$scope.$emit('')
+        // alert('yay' + JSON.stringify(tableMod.getPaths()));
+      
+        $scope.$emit('facete:refresh');
+      
+//        var columnDefs = tableMod.getColumnDefs();
+//        _(columnDefs).each(function(columnDef) {
+          
+//        });
+      
+//        tableMod.addColumnDef(null, new ns.ColumnDefPath(path));
+      //alert('yay ' + path);
+        };
+      
+  //  $scope.$on('facete:refresh', function() {
+//        $scope.refresh();
+  //  });
+}])
+
+/**
+ * The actual dependencies are:
+ * - sparqlServiceFactory
+ * - facetTreeConfig
+ * - labelMap (maybe this should be part of the facetTreeConfig) 
+ */
+.directive('facetTree', function($parse) {
+    return {
+        restrict: 'EA',
+        replace: true,
+        templateUrl: 'template/facettree/facet-tree-item.html',
+        transclude: false,
+        require: 'facetTree',
+        scope: {
+            sparqlService: '=',
+            facetTreeConfig: '=',
+            onSelect: '&select'
+        },
+        controller: 'FacetTreeCtrl',
+        compile: function(elm, attrs) {
+            return function link(scope, elm, attrs, controller) {
+            };
+        }
+    };
+})
+
+;
+
+angular.module('ui.jassa.facetvaluelist', [])
 
 /**
  * Controller for the SPARQL based FacetTree
@@ -229,171 +394,6 @@ angular.module('ui.jassa.facetValueList', [])
             onSelect: '&select'
         },
         controller: 'FacetValueListCtrl',
-        compile: function(elm, attrs) {
-            return function link(scope, elm, attrs, controller) {
-            };
-        }
-    };
-})
-
-;
-
-angular.module('ui.jassa.facettree', [])
-
-/**
- * Controller for the SPARQL based FacetTree
- * Supports nested incoming and outgoing properties
- *
- */
-.controller('FacetTreeCtrl', ['$rootScope', '$scope', '$q', function($rootScope, $scope, $q) {
-        
-    var self = this;
-      
-      
-    var updateFacetTreeService = function() {
-        var isConfigured = $scope.sparqlService && $scope.facetTreeConfig;
-        //debugger;
-        $scope.facetTreeService = isConfigured ? Jassa.facete.FaceteUtils.createFacetTreeService($scope.sparqlService, $scope.facetTreeConfig, null) : null;
-    };
-    
-    var update = function() {
-        updateFacetTreeService();
-        //controller.refresh();
-        self.refresh();
-    };
-    
-    $scope.$watch('sparqlService', function() {
-    //console.log('args', $scope.sparqlService);
-        update();
-    });
-    
-    $scope.$watch('facetTreeConfig.hashCode()', function() {
-        update();
-    }, true);
-              
-      
-    $scope.doFilter = function(path, filterString) {
-        $scope.facetTreeConfig.getPathToFilterString().put(path, filterString);
-        self.refresh();
-    };
-    
-    self.refresh = function() {
-                  
-        var facet = $scope.facet;
-        var startPath = facet ? facet.item.getPath() : new facete.Path();
-    
-        if($scope.facetTreeService) {
-          
-            var facetTreeTagger = Jassa.facete.FaceteUtils.createFacetTreeTagger($scope.facetTreeConfig.getPathToFilterString());
-    
-            //console.log('scopefacets', $scope.facet);             
-            var promise = $scope.facetTreeService.fetchFacetTree(startPath);
-              
-            Jassa.sponate.angular.bridgePromise(promise, $q.defer(), $rootScope).then(function(data) {
-                facetTreeTagger.applyTags(data);
-                $scope.facet = data;
-            });
-    
-        } else {
-            $scope.facet = null;
-        }
-    };
-              
-    $scope.toggleCollapsed = function(path) {
-        Jassa.util.CollectionUtils.toggleItem($scope.facetTreeConfig.getExpansionSet(), path);
-          
-        var val = $scope.facetTreeConfig.getExpansionMap().get(path);
-        if(val == null) {
-            $scope.facetTreeConfig.getExpansionMap().put(path, 1);
-        }
-          
-        self.refresh();
-    };
-      
-    $scope.selectIncoming = function(path) {
-        console.log('Incoming selected at path ' + path);
-        if($scope.facetTreeConfig) {
-            var val = $scope.facetTreeConfig.getExpansionMap().get(path);
-            if(val != 2) {
-                $scope.facetTreeConfig.getExpansionMap().put(path, 2);
-                self.refresh();
-            }
-        }
-    };
-      
-    $scope.selectOutgoing = function(path) {
-        console.log('Outgoing selected at path ' + path);
-        if($scope.facetTreeConfig) {
-            var val = $scope.facetTreeConfig.getExpansionMap().get(path);
-            if(val != 1) {
-                $scope.facetTreeConfig.getExpansionMap().put(path, 1);
-                self.refresh();
-            }
-        }
-    };
-      
-      
-    $scope.selectFacetPage = function(page, facet) {
-        var path = facet.item.getPath();
-        var state = $scope.facetTreeConfig.getFacetStateProvider().getFacetState(path);
-        var resultRange = state.getResultRange();
-          
-        console.log('Facet state for path ' + path + ': ' + state);
-            var limit = resultRange.getLimit() || 0;
-              
-            var newOffset = limit ? (page - 1) * limit : null;
-              
-            resultRange.setOffset(newOffset);
-            
-            self.refresh();
-        };
-          
-        $scope.toggleSelected = function(path) {
-            $scope.onSelect({path: path});
-        };
-  
-        $scope.toggleTableLink = function(path) {
-            //$scope.emit('facete:toggleTableLink');
-        tableMod.togglePath(path);
-      
-        //$scope.$emit('')
-        // alert('yay' + JSON.stringify(tableMod.getPaths()));
-      
-        $scope.$emit('facete:refresh');
-      
-//        var columnDefs = tableMod.getColumnDefs();
-//        _(columnDefs).each(function(columnDef) {
-          
-//        });
-      
-//        tableMod.addColumnDef(null, new ns.ColumnDefPath(path));
-      //alert('yay ' + path);
-        };
-      
-  //  $scope.$on('facete:refresh', function() {
-//        $scope.refresh();
-  //  });
-}])
-
-/**
- * The actual dependencies are:
- * - sparqlServiceFactory
- * - facetTreeConfig
- * - labelMap (maybe this should be part of the facetTreeConfig) 
- */
-.directive('facetTree', function($parse) {
-    return {
-        restrict: 'EA',
-        replace: true,
-        templateUrl: 'template/facettree/facet-tree-item.html',
-        transclude: false,
-        require: 'facetTree',
-        scope: {
-            sparqlService: '=',
-            facetTreeConfig: '=',
-            onSelect: '&select'
-        },
-        controller: 'FacetTreeCtrl',
         compile: function(elm, attrs) {
             return function link(scope, elm, attrs, controller) {
             };
