e6554ac9105f520773ca18f492d6c33aa0bdca3a
Jenkins (AKSW) <cstadler@informatik.uni-leipzig.de>
2014-02-21 18:11:26 +0100

    Updating version 0.0.1-SNAPSHOT with tag v0.0.1-SNAPSHOT

--- jassa-ui-angular.js
+++ jassa-ui-angular.js
@@ -2,7 +2,7 @@
  * jassa-ui-angular
  * https://github.com/GeoKnow/Jassa-UI-Angular
 
- * Version: 0.0.1-SNAPSHOT - 2014-02-20
+ * Version: 0.0.1-SNAPSHOT - 2014-02-21
  * License: MIT
  */
 angular.module("ui.jassa", ["ui.jassa.constraint-list","ui.jassa.facet-tree","ui.jassa.facet-value-list"]);
