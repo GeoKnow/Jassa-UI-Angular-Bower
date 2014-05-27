fc762831e090ddb8823eeae0bd522191764c8b73
cstadler <cstadler@informatik.uni-leipzig.de>
2014-05-28 00:07:05 +0200

    Updating version 0.0.3-SNAPSHOT with tag v0.0.3-SNAPSHOT

--- jassa-ui-angular-tpls.js
+++ jassa-ui-angular-tpls.js
@@ -2,10 +2,10 @@
  * jassa-ui-angular
  * https://github.com/GeoKnow/Jassa-UI-Angular
 
- * Version: 0.0.3-SNAPSHOT - 2014-05-27
+ * Version: 0.0.3-SNAPSHOT - 2014-05-28
  * License: MIT
  */
-angular.module("ui.jassa", ["ui.jassa.tpls", "ui.jassa.constraint-list","ui.jassa.facet-tree","ui.jassa.facet-typeahead","ui.jassa.facet-value-list","ui.jassa.resizable","ui.jassa.sparql-table","ui.jassa.template-list"]);
+angular.module("ui.jassa", ["ui.jassa.tpls", "ui.jassa.constraint-list","ui.jassa.facet-tree","ui.jassa.facet-typeahead","ui.jassa.facet-value-list","ui.jassa.pointer-events-scroll-fix","ui.jassa.resizable","ui.jassa.sparql-table","ui.jassa.template-list"]);
 angular.module("ui.jassa.tpls", ["template/constraint-list/constraint-list.html","template/facet-tree/facet-dir-content.html","template/facet-tree/facet-dir-ctrl.html","template/facet-tree/facet-tree-item.html","template/facet-value-list/facet-value-list.html","template/sparql-table/sparql-table.html","template/template-list/template-list.html"]);
 angular.module('ui.jassa.constraint-list', [])
 
@@ -694,6 +694,62 @@ angular.module('ui.jassa.facet-value-list', [])
 })
 
 ;
+
+angular.module('ui.jassa.pointer-events-scroll-fix', [])
+
+/**
+ * Scrollbars on overflow divs with pointer-events: none are not clickable on chrome/chromium.
+ * This directive sets pointer-events to auto when scrollbars are needed and thus assumed to be visible.
+ *
+ */
+.directive('pointerEventsScrollFix', function() {
+    return {
+        restrict: 'A',
+        //scope: 
+        compile: function() {
+            return {
+                post: function(scope, elem, attrs) {
+
+                    // TODO Registering (jQuery) plugins in a directive is actually an anti-pattern - either get rid of this or move the plugin to a common location
+                    if(!jQuery.fn.hasScrollBar) {
+                        jQuery.fn.hasScrollBar = function() {
+                            var el = this.get(0);
+                            if(!el) {
+                                console.log('Should not happen');
+                                return false;
+                            }
+
+                            var result = el.scrollHeight > el.clientHeight || el.scrollWidth > el.clientWidth;
+                            //console.log('Checked scrollbar state: ', result);
+                            return result;
+                        };
+                    }
+                    
+                    var backup = null;
+                    
+                    scope.$watch(
+                        function () { return jQuery(elem).hasScrollBar(); },
+                        function (hasScrollBar) {
+                            console.log('Scrollbar state: ', hasScrollBar, backup);
+                            if(hasScrollBar) {
+                                if(!backup) {
+                                    backup = elem.css('pointer-events');
+                                    elem.css('pointer-events', 'auto');
+                                }
+                            } else if(backup) {
+                                elem.css('pointer-events', backup);
+                                backup = null;
+                            }
+                        }
+                    );
+                }
+            };
+        }
+    };
+})
+
+;
+
 
 angular.module('ui.jassa.resizable', [])
 
