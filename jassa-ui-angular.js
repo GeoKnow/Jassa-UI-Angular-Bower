97666bb02975f761c822eaff1b9c7fbd690bc46d
Jenkins (AKSW) <cstadler@informatik.uni-leipzig.de>
2014-08-07 00:21:16 +0200

    Updating version 0.0.4-SNAPSHOT with tag v0.0.4-SNAPSHOT

--- jassa-ui-angular.js
+++ jassa-ui-angular.js
@@ -2,7 +2,7 @@
  * jassa-ui-angular
  * https://github.com/GeoKnow/Jassa-UI-Angular
 
- * Version: 0.0.4-SNAPSHOT - 2014-08-06
+ * Version: 0.0.4-SNAPSHOT - 2014-08-07
  * License: MIT
  */
 angular.module("ui.jassa", ["ui.jassa.constraint-list","ui.jassa.facet-tree","ui.jassa.facet-typeahead","ui.jassa.facet-value-list","ui.jassa.pointer-events-scroll-fix","ui.jassa.resizable","ui.jassa.sparql-grid","ui.jassa.template-list"]);
