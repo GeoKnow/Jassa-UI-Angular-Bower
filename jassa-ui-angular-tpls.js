772a6f6027e6530b1a932ded5c40ea509d4dd085
Jenkins (AKSW) <cstadler@informatik.uni-leipzig.de>
2014-09-18 02:31:00 +0200

    Updating version 0.0.4-SNAPSHOT with tag v0.0.4-SNAPSHOT

--- jassa-ui-angular-tpls.js
+++ jassa-ui-angular-tpls.js
@@ -1429,6 +1429,7 @@ angular.module("template/constraint-list/constraint-list.html", []).run(["$templ
 
 angular.module("template/facet-tree/facet-dir-content.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/facet-tree/facet-dir-content.html",
+    "\n" +
     "<!-- ng-show=\"dirset.pageCount > 1 || dirset.children.length > 5\" -->\n" +
     "\n" +
     "\n" +
